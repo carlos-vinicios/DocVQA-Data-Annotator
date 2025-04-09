718d37ab9ce423bc8c25550fb35a2aafa5681d73
Carlos Vinicios Martins Rocha <vinicios.martins2014@gmail.com>
2025-04-09 15:56:11 -0300

    Update mongo_connection.py

--- utils/mongo_connection.py
+++ utils/mongo_connection.py
@@ -2,8 +2,8 @@ import pymongo
 import os
 
 def generate_connection(collection_name: str):
-    conn_str = "mongodb+srv://root_user:CNfjWrKJlIh9pnSj@docvqa.olokopd.mongodb.net/DOC_VQA?retryWrites=true&w=majority" #os.environ.get("MONGODB_URI")
-    db_name = "DOC_VQA"#os.environ.get("MONGODB_DB_NAME")
+    conn_str = os.environ.get("MONGODB_URI")
+    db_name = os.environ.get("MONGODB_DB_NAME")
 
     print(db_name)
     client = pymongo.MongoClient(
